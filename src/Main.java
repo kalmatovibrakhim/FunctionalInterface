31dc016a78f1cdd8c152b870bc9da9d85fadeaf1
kalmatovibrakhim <kalmatovibrakhim96kg@gmail.com>
2022-11-23 20:44:17 +0300

    3commit

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,27 @@ import java.util.function.UnaryOperator;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(sqrt(30));
+        System.out.println(sqrt().apply(46d));
 
     }
-   /*
-    public static UnaryOperator<Double> sqrt(double x){
+
+    public static UnaryOperator<Double> sqrt(){
         UnaryOperator<Double>unaryOperator=(a)->Math.sqrt(a);
-        System.out.println(unaryOperator.apply(x));
+//        System.out.println(unaryOperator.apply(x));
         return unaryOperator;
 
     };
 
-    */
-    public static double sqrt(double x){
-        UnaryOperator<Double>unaryOperator= Math::sqrt;
-        return unaryOperator.apply(x);
 
-    };
+//    public static double sqrt(double x){
+//        UnaryOperator<Double>unaryOperator= Math::sqrt; // (a)->Math.sqrt(a);
+//        UnaryOperator<Double>unaryOperator1 = new UnaryOperator<Double>() {
+//            @Override
+//            public Double apply(Double aDouble) {
+//                return null;
+//            }
+//        };
+//
+//        return x;
+//    };
 }
